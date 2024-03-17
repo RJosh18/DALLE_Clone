35b0c562ce18658e2d91ab8980f4b8513b6c381d
RJosh18 <riajoshi318@gmail.com>
2024-03-18 04:01:35 +0530

    feat FormField

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,7 +1,77 @@
 import React from "react";
 
-const FormField = () => {
-  return <div>FormField</div>;
-};
+const FormField = ({
+  LabelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => (
+  <div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "centre",
+        gap: "2px",
+        marginBottom: "2px",
+      }}
+    >
+      <label
+        htmlFor={name}
+        style={{
+          display: "block",
+          fontSize: "0.875rem",
+          fontWeight: "500",
+          color: "#111827",
+        }}
+      >
+        {LabelName}
+      </label>
+      {isSurpriseMe && (
+        <button
+          type="button"
+          onClick={handleSurpriseMe}
+          style={{
+            fontWeight: "600",
+            fontSize: "0.75rem",
+            background: "#ECECF1",
+            paddingTop: "0.25rem",
+            paddingBottom: "0.25rem",
+            paddingLeft: "0.5rem",
+            paddingRight: "0.5rem",
+            borderRadius: "5px",
+            color: "#000000",
+          }}
+        >
+          Surprise me
+        </button>
+      )}
+    </div>
+    <input
+      type={type}
+      id={name}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      required
+      style={{
+        background: "#F9FAFB",
+        borderColor: "#D1D5DB",
+        color: "#111827",
+        fontSize: "0.875rem",
+        borderRadius: "0.375rem",
+        border: "1px solid #D1D5DB", // Added border property
+        outline: "none",
+        display: "block",
+        width: "100%",
+        padding: "0.75rem",
+      }}
+    />
+  </div>
+);
 
 export default FormField;
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import preview from "../assets/preview.png";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-  return <div>Create Post</div>;
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+  const handleChange = (e) => {};
+  const handleSurpriseMe = () => {};
+  return (
+    <section
+      style={{ maxWidth: "80rem", marginLeft: "auto", marginRight: "auto" }}
+    >
+      <div>
+        <h1
+          style={{
+            fontStyle: "bold",
+            color: "#222328",
+            fontWeight: "900",
+            fontSize: "25px",
+          }}
+        >
+          Create
+        </h1>
+        <p
+          style={{
+            marginTop: "0.5rem",
+            color: "666e75",
+            fontSize: "14px",
+            maxWidth: "650px",
+          }}
+        >
+          Generate an imaginative image through DALL-E AI and share it with the
+          community
+        </p>
+      </div>
+      <form
+        style={{ marginTop: "4rem", maxWidth: "48rem" }}
+        onSubmit={handleSubmit}
+      >
+        <div
+          style={{
+            display: "flex",
+            marginTop: "1.25rem",
+            flexDirection: "column",
+          }}
+        >
+          <FormField
+            LabelName="Your name"
+            type="text"
+            name="name"
+            placeholder="Rachel Bing"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            LabelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="a pencil and watercolor drawing of a bright city in the future with flying cars"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
